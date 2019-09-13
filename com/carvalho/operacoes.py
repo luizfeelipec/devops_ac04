e28ddb2f02adaca9bc7185fa7fbb1a91563c7d08
luizfeelipec <luizfeelipec1@hotmail.com>
2019-09-13 20:58:47 -0300

    Update operacoes.py

--- com/carvalho/operacoes.py
+++ com/carvalho/operacoes.py
@@ -1,13 +1,36 @@
-class Operacoes():
+def ehprimo(num):
+    '''
+    Função que retorna True se um número é primo.
+    E False caso o contrário.
+    '''
+    for i in range(2, num//2):
+        if num % i == 0:
+            return False
+    return True
 
-	def soma(self, valores):
-		val = 0
-		for v in valores:
-			val = val + v
-		return val
-	
-	def subtracao(self, valores):
-		val = 0
-		for v in valores:
-			val = val - v
-		return val
+
+def listaprimos2(num):
+    primos = []
+    for i in range(2, num):
+        if ehprimo(i):
+            primos.append(i)
+    return primos
+
+
+def listaprimos(num):
+    '''
+    Função que lista os números primos menores que num.
+    '''
+    primos = []
+    for i in range(2, num):
+        primo = True
+        for j in range(2, i):
+            if i % j == 0:
+                primo = False
+        if primo:
+            primos.append(i)
+    return primos
+
+
+if __name__ == '__main__':
+    print(listaprimos(150))
